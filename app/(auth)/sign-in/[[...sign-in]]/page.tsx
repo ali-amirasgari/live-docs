9de759e5fea1721f512a1468a0788a42e994d976
amirasgari.a <aliamirasgari2002@gmail.com>
2025-03-09 14:56:58 +0330

    Add some changes in sign in page and add keys in sample file to have it in repository

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,15 @@
-import { SignIn } from "@clerk/nextjs";
+"use client";
+
+import { SignIn, useUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 function SignInPage() {
+  // const { isSignedIn } = useUser();
+
+  // if (isSignedIn) {
+  //   return redirect("/");
+  // }
+
   return (
     <main className="auth-page">
       <SignIn />
